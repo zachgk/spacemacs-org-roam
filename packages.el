bdcc73c7c4aaaebd667721fff0fcbcbf827e0ed3
Zach Kimberg <zachary@kimberg.com>
2020-05-16 05:26:07 -0700

    Remove dailies-today in favor of org-journal

--- packages.el
+++ packages.el
@@ -15,14 +15,12 @@
       (spacemacs/declare-prefix "ar" "org-roam")
       (spacemacs/set-leader-keys
        "arl" 'org-roam
-       "art" 'org-roam-dailies-today
        "arf" 'org-roam-find-file
        "arg" 'org-roam-graph)
 
       (spacemacs/declare-prefix-for-mode 'org-journal-mode "mr" "org-roam")
       (spacemacs/set-leader-keys-for-major-mode 'org-journal-mode
                                                 "rl" 'org-roam
-                                                "rt" 'org-roam-dailies-today
                                                 "rb" 'org-roam-switch-to-buffer
                                                 "rf" 'org-roam-find-file
                                                 "ri" 'org-roam-insert
@@ -30,7 +28,6 @@
       (spacemacs/declare-prefix-for-mode 'org-mode "mr" "org-roam")
       (spacemacs/set-leader-keys-for-major-mode 'org-mode
                                                 "rl" 'org-roam
-                                                "rt" 'org-roam-dailies-today
                                                 "rb" 'org-roam-switch-to-buffer
                                                 "rf" 'org-roam-find-file
                                                 "ri" 'org-roam-insert
