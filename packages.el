588bfc655ef9ea08615997513b6a34c5d836c9f0
Zach Kimberg <zachary@kimberg.com>
2021-01-11 23:10:10 -0800

    Update as org-roam is in upstream spacemacs org layer

--- packages.el
+++ packages.el
@@ -1,61 +1,28 @@
 (defconst org-roam-packages
-  '(org-roam
-    org-roam-bibtex
-    (company-org-roam :location (recipe :fetcher github :repo "org-roam/company-org-roam"))
+  '(org-roam-bibtex
     org-noter
     (org-fc :location local)))
 
-(defun org-roam/init-org-roam ()
-  (use-package org-roam
-    :hook
-    (after-init . org-roam-mode)
-    :init
-    (progn
-      (setq org-roam-title-include-subdirs t)
-
-      (spacemacs/declare-prefix "ar" "org-roam")
-      (spacemacs/set-leader-keys
-        "arl" 'org-roam
-        "arf" 'org-roam-find-file
-        "arg" 'org-roam-graph)
-
-      (spacemacs/declare-prefix-for-mode 'org-journal-mode "mr" "org-roam")
-      (spacemacs/set-leader-keys-for-major-mode 'org-journal-mode
-        "rl" 'org-roam
-        "rb" 'org-roam-switch-to-buffer
-        "rf" 'org-roam-find-file
-        "rF" 'org-roam-find-file-immediate
-        "ri" 'org-roam-insert
-        "rI" 'org-roam-insert-immediate
-        "rR" 'org-roam-random-note
-        "rg" 'org-roam-graph)
-      (spacemacs/declare-prefix-for-mode 'org-mode "mr" "org-roam")
-      (spacemacs/set-leader-keys-for-major-mode 'org-mode
-        "rl" 'org-roam
-        "rb" 'org-roam-switch-to-buffer
-        "rf" 'org-roam-find-file
-        "rF" 'org-roam-find-file-immediate
-        "ri" 'org-roam-insert
-        "rI" 'org-roam-insert-immediate
-        "rR" 'org-roam-random-note
-        "rg" 'org-roam-graph)
-      )))
-
 (defun org-roam/init-org-roam-bibtex ()
   (use-package org-roam-bibtex
+    :after org-roam
     :hook
-    (org-roam-mode . org-roam-bibtex-mode)))
-
-(defun org-roam/init-company-org-roam ()
-  (use-package company-org-roam
+    (org-roam-mode . org-roam-bibtex-mode)
+    :bind (:map org-mode-map
+               (("C-c n a" . orb-note-actions)))
     :init
     (progn
-      (spacemacs|add-company-backends :backends 'company-org-roam :modes org-mode)
-      )))
+      (require 'org-roam-bibtex)
+      (add-hook 'after-init-hook #'org-roam-bibtex-mode)
+      (setq org-ref-notes-function 'orb-edit-notes)
+      (define-key org-roam-bibtex-mode-map (kbd "C-c n a") #'orb-note-actions)
+      )
+    ))
 
 
 (defun org-roam/init-org-fc ()
   (use-package org-fc
+    :load-path "/data/projects/emacs/org-fc"
     :config
     (require 'org-fc-hydra)
     :init
@@ -77,10 +44,22 @@
         "n" 'org-noter
         ))))
 
-(defun org-roam/post-init-org ()
-  (require 'org-protocol)
-  (add-to-list 'org-modules 'org-protocol))
-
 (defun org-roam/post-init-org-roam ()
-  (require 'org-roam-protocol)
-  (add-to-list 'org-modules 'org-roam-protocol))
+  (spacemacs/set-leader-keys-for-major-mode 'org-journal-mode
+    "rR" 'org-roam-random-note
+    )
+  (spacemacs/declare-prefix-for-mode 'org-mode "mr" "org-roam")
+  (spacemacs/set-leader-keys-for-major-mode 'org-mode
+    "rR" 'org-roam-random-note
+    )
+  (spacemacs/declare-prefix-for-mode 'org-journal-mode "mr" "org-roam")
+  (spacemacs/set-leader-keys-for-major-mode 'org-journal-mode
+    "rb" 'org-roam-switch-to-buffer
+    "rdy" 'org-roam-dailies-yesterday
+    "rdt" 'org-roam-dailies-today
+    "rdT" 'org-roam-dailies-tomorrow
+    "rf" 'org-roam-find-file
+    "rg" 'org-roam-graph
+    "ri" 'org-roam-insert
+    "rI" 'org-roam-insert-immediate
+    "rl" 'org-roam))
